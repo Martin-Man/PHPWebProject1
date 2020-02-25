caa12fa853451fa73f0ddd1bc2e58a998b7b413d
Martin Maniak <martin.maniak975@edu.ku.sk>
2020-02-25 15:46:41 +0100

    index uprava

--- PHPWebProject1/index.php
+++ PHPWebProject1/index.php
@@ -1,4 +1,12 @@
 <?php
 
-echo 'Hello Framework!';
-echo 'Hello Framework!';
+include 'pages/header.php';							//vloží hlavièku stránky
+include 'connect.php';								//vloží súbor na spojenie s databázou
+
+if (isset($_GET['link'])) include 'pages/'.$_GET["link"];	//vloží obsah požadovanej stránky (premennú "link" vkladáme v adresnom riadku)
+else include 'pages/home.php';						//alebo vloží defaultnú domácu stránku
+
+$conn->close();
+include 'pages/footer.php';							//vloží pätièku stránky
+
+?>
